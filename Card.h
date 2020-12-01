185b87b01ae50521fa7e8d57bab51cc20eb3bb06
Korbarus <korbarus2951@gmail.com>
2020-12-01 00:39:07 +0000

    Initial commit

---
+++ Card.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <string>
+#include <stdexcept>
+
+class Card
+{
+	std::string cardName;
+	std::string upAttributes[5];
+	std::string revAttributes[5];
+
+public:
+	std::string getCardName();
+	std::string getUpAttr();
+	std::string getrevAttr();
+
+};
+
+std::string Card::getCardName() 
+{
+	return cardName;
+}
+
+std::string Card::getUpAttr()
+{
+
+	// Concatenates all upright attributes into a single string for output purposes.
+	std::string allAttr;
+	
+	for (int i = 0; i < 5; i++) 
+	{
+		try 
+		{
+			allAttr = allAttr + upAttributes[i];
+		}
+		// If there's less than 5 attributes, we skip over it.
+		catch (const std::out_of_range& oor) 
+		{
+			continue;
+		}
+		
+	}
+
+	return allAttr;
+}
+
+std::string Card::getrevAttr()
+{
+
+	// Concatenates all reversed attributes into a single string for output purposes.
+	std::string allAttr;
+
+	for (int i = 0; i < 5; i++)
+	{
+		try
+		{
+			allAttr = allAttr + revAttributes[i];
+		}
+		// If there's less than 5 attributes, we skip over it.
+		catch (const std::out_of_range& oor)
+		{
+			continue;
+		}
+
+	}
+
+	return allAttr;
+}
